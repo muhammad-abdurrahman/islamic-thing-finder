fd3b71298c9d40c2713b23ef3a6256e4a4f0b0b4
Muhammad Abdurrahman <muhammad.abdurrahman.uk@gmail.com>
2020-12-25 16:56:48 +0000

    initialize repository

---
+++ src/main/java/com/github/muhammadabdurrahman/islamicthingfinderapi/IslamicThingFinderApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.muhammadabdurrahman.islamicthingfinderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IslamicThingFinderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IslamicThingFinderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/muhammadabdurrahman/islamicthingfinderapi/IslamicThingFinderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.muhammadabdurrahman.islamicthingfinderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IslamicThingFinderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
